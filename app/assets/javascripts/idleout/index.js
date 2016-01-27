565f099d7662cab11fca04faba020b393f89c8ac
Noah Holmes <nh118@duke.edu>
2016-01-27 14:55:37 -0500

    changed timeout to be set lower

--- app/assets/javascripts/idleout/index.js
+++ app/assets/javascripts/idleout/index.js
@@ -2,13 +2,13 @@ var idleTime = 0;
 var alerted = true;
 $(document).ready(function () {
   if(alerted === true) {
-    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+    var idleInterval = setInterval(timerIncrement, 20000); // 1 minute
   }
 });
 
 function timerIncrement() {
   idleTime++;
-  if (idleTime > 50 && alerted === true) { // 50 minutes
+  if (idleTime > 1 && alerted === true) { // 50 minutes
     alerted = false
     alert("Your page has timed out.  Please save/copy your information elsewhere before you are redirected to our login server.")
   }
