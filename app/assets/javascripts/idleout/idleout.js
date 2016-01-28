9a68045498ed93cb223a2b1f60208b7bba658b22
Noah Holmes <nh118@duke.edu>
2016-01-28 11:40:56 -0500

    changed input to respond to minutes instead of timer increment

--- app/assets/javascripts/idleout/idleout.js
+++ app/assets/javascripts/idleout/idleout.js
@@ -6,16 +6,16 @@ idleOut.pageTimeOut = function(timeout) {
 
 idleOut.pageTimeOut.prototype.start = function() {
   var idleTime = 0;
-  var increment = this.timeout
+  var minutes = this.timeout
   var alerted = true;
 
   if(alerted === true) {
-    var idleInterval = setInterval(timerIncrement, increment); // 1 minute
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
   }
 
   function timerIncrement() {
     idleTime++;
-    if (idleTime > 1 && alerted === true) { // 50 minutes
+    if (idleTime > minutes && alerted === true) { // 50 minutes
       alerted = false
       alert("Your page has timed out.  Please save/copy your information elsewhere before you are redirected to our login server.")
     }
