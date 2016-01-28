1437c2e988f4bc506fcb1c606f9643687dc295c2
Noah Holmes <nh118@duke.edu>
2016-01-28 08:50:56 -0500

    Changed format of JS code

--- app/assets/javascripts/idleout/index.js
+++ app/assets/javascripts/idleout/index.js
@@ -1,15 +1,16 @@
-var idleTime = 0;
-var alerted = true;
 $(document).ready(function () {
+  var idleTime = 0;
+  var alerted = true;
+
   if(alerted === true) {
     var idleInterval = setInterval(timerIncrement, 20000); // 1 minute
   }
-});
 
-function timerIncrement() {
-  idleTime++;
-  if (idleTime > 1 && alerted === true) { // 50 minutes
-    alerted = false
-    alert("Your page has timed out.  Please save/copy your information elsewhere before you are redirected to our login server.")
+  function timerIncrement() {
+    idleTime++;
+    if (idleTime > 1 && alerted === true) { // 50 minutes
+      alerted = false
+      alert("Your page has timed out.  Please save/copy your information elsewhere before you are redirected to our login server.")
+    }
   }
-}
+});
